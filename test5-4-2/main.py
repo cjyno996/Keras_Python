f6cee8801be295d9817e3e9c1a0e923efde45b2d
PC_LAB <1095277786@qq.com>
2021-01-25 16:57:30 +0800

    first commit

---
+++ test5-4-2/main.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+from keras.applications import VGG16
+from keras import backend as K
+
+tf.compat.v1.disable_eager_execution()  # 否则在tensorflow 2.x版本会报错
+
+model = VGG16(weights='imagenet',
+              include_top=False)
+
+
+def deprocess_image(x):
+    x -= x.mean()
+    x /= (x.std() + 1e-5)
+    x *= 0.1
+    x += 0.5
+    x = np.clip(x, 0, 1)
+    x *= 255
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+
+def generate_pattern(layer_name, filter_index, size=150):
+    layer_output = model.get_layer(layer_name).output
+    loss = K.mean(layer_output[:, :, :, filter_index])
+    grads = K.gradients(loss, model.input)[0]
+    grads /= (K.sqrt(K.mean(K.square(grads))) + 1e-5)
+    iterate = K.function([model.input], [loss, grads])
+    input_img_data = np.random.random((1, size, size, 3)) * 20 + 128.
+    step = 1.
+    for i in range(40):
+        loss_value, grads_value = iterate([input_img_data])
+        input_img_data += grads_value * step
+
+    img = input_img_data[0]
+    return deprocess_image(img)
+
+
+plt.imshow(generate_pattern('block3_conv1', 0))
+plt.show()
+
+layer_name = 'block4_conv1'
+size = 64
+margin = 5
+results = np.zeros((8 * size + 7 * margin, 8 * size + 7 * margin, 3), dtype = 'uint8')
+for i in range(8):
+    for j in range(8):
+        filter_img = generate_pattern(layer_name, i + (j * 8), size=size)
+        horizontal_start = i * size + i * margin
+        horizontal_end = horizontal_start + size
+        vertical_start = j * size + j * margin
+        vertical_end = vertical_start + size
+        results[horizontal_start: horizontal_end,
+        vertical_start: vertical_end, :] = filter_img
+plt.figure(figsize=(20, 20))
+plt.imshow(results)
+plt.show()
